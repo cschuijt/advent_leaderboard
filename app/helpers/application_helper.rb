e1f6328627fc6e5dceb305990d407d5313fb28fb
Casper Schuijt <casperschuijt@gmail.com>
2023-11-21 23:04:00 +0000

    Application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,14 @@ module ApplicationHelper
       "#{title} | Codam AoC Leaderboard"
     end
   end
+
+  def page_year
+    if @year
+      @year.number
+    elsif params[:year]
+      params[:year]
+    else
+      Time.now.year
+    end
+  end
 end
