73976273d568e71be5cff290034da18d302da179
Casper Schuijt <casperschuijt@gmail.com>
2023-11-22 01:05:45 +0000

    Year overview should have a current day

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
     @year = Year.last # Assuming the most recent year was created last...
+    @today = @year.days.where(open: true).order(number: :desc).first
     render 'years/show'
   end
 end
--- app/controllers/years_controller.rb
+++ app/controllers/years_controller.rb
@@ -5,5 +5,6 @@ class YearsController < ApplicationController
 
   def show
     @year = Year.find_by!(number: params[:year])
+    @today = @year.days.where(open: true).order(number: :desc).first
   end
 end
