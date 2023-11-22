4d913376e080922907e6bb459771c48c638c45c9
Casper Schuijt <casperschuijt@gmail.com>
2023-11-22 01:18:36 +0000

    Backup homepage when no years have been created

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
   def home
     @year = Year.last # Assuming the most recent year was created last...
+    if @year.nil?
+      render 'pages/home'
+      return
+    end
     @today = @year.days.where(open: true).order(number: :desc).first
     render 'years/show'
   end
