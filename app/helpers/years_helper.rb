d8933e692656a1576f127f6fa72bc04112d27a1f
Casper Schuijt <casperschuijt@gmail.com>
2023-11-23 20:59:41 +0000

    Days overview in year show page

--- app/helpers/years_helper.rb
+++ app/helpers/years_helper.rb
@@ -1,2 +1,18 @@
 module YearsHelper
+  def bg_class_for_day(day)
+    if logged_in?
+      participant = day.year.participants.find_by(user: current_user)
+      star = day.stars.where(participant: participant).order(index: :desc).first
+
+      if !star
+        return nil
+      elsif star.index == 2
+        return 'bg-gold'
+      else
+        return 'bg-silver'
+      end
+    else
+      return nil
+    end
+  end
 end
