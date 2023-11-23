91cc7351bbcb7c851e97e911a9cd65af4394724f
Casper Schuijt <casperschuijt@gmail.com>
2023-11-23 00:49:32 +0000

    Bootstrap javascript in asset pipeline

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require bootstrap-sprockets
 import "@hotwired/turbo-rails"
 import "controllers"
