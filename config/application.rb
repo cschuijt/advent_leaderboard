2c8562b87237b4b3500359819e84bfd31ff1439e
Casper Schuijt <casperschuijt@gmail.com>
2023-11-23 22:23:24 +0000

    Time zones and time formatting

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module AdventLeaderboard
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Europe/Amsterdam"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
