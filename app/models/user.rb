ed2b8f81378faaea740eebb28e989aff23a5bad8
Casper Schuijt <casperschuijt@gmail.com>
2023-11-26 14:32:33 +0000

    Instantiate users from 42 API response

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,21 @@ class User < ApplicationRecord
 
   before_validation :remove_hashtags
 
+  extend FortytwoIntra
+
+  # Instantiates a new user from data we get out of the 42 API for
+  # a specific username. This user will not be saved to the database
+  # yet, so we can add their AoC ID to it beforehand.
+  def self.from_42_api(username)
+    api_response = fortytwo_api_client.get()
+
+    return User.new(
+      username: api_response.login,
+      full_name: api_response.usual_full_name,
+      photo_url: api_response.image.versions.medium
+    )
+  end
+
   private
 
   def remove_hashtags
