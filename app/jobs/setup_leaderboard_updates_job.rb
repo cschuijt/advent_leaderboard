0b8b7a16ed1dad6896e1c1571bd8ba619534385a
Casper Schuijt <casperschuijt@gmail.com>
2023-11-28 23:25:14 +0000

    Change job schedule and structure

---
+++ app/jobs/setup_leaderboard_updates_job.rb
@@ -0,0 +1,21 @@
+class SetupLeaderboardUpdatesJob < ApplicationJob
+  def perform(options = {year: Time.now.year, days: 25, min_interval: 20})
+    GoodJob::Bulk.enqueue do
+      # We start running at 5AM UTC on December 1st and end after the given
+      # amount of days. For each minute interval given, we will enqueue a job
+      # to update this leaderboard, as far as those times are in the future.
+      start_time = DateTime.new(options[:year], 12, 1, 5, 0)
+      end_time   = start_time + options[:days].days
+
+      time = start_time
+      while time <= end_time
+        # We do not need to enqueue jobs that are in the past
+        if time > Time.now
+          UpdateLeaderboardJob.set(wait_until: time).perform_later
+        end
+
+        time = time + options[:min_interval].minutes
+      end
+    end
+  end
+end
