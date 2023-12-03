9d886f263349c49995f2c29d4d585d362c139b69
Casper Schuijt <casperschuijt@gmail.com>
2023-12-03 12:48:30 +0000

    Change OpenDayJob to update everyone's position

--- app/jobs/open_day_job.rb
+++ app/jobs/open_day_job.rb
@@ -2,8 +2,13 @@ class OpenDayJob < ApplicationJob
   queue_as :default
 
   def perform(year, day)
-    day = Year.find_by!(number: year).days.find_by!(number: day)
+    year = Year.find_by!(number: year)
+    day = year.days.find_by!(number: day)
 
     day.update!(open: true)
+
+    year.participants.order(score: :desc).each.with_index do |p, i|
+      p.update!(position_yesterday: i)
+    end
   end
 end
