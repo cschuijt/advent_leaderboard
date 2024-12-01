2e9ddd7ab4956a7b10ec53320b0038778d2f08e4
Casper Schuijt <casper@cschuijt.nl>
2024-12-01 16:22:33 +0000

    include stars and user with participants

--- app/controllers/years_controller.rb
+++ app/controllers/years_controller.rb
@@ -1,7 +1,7 @@
 class YearsController < ApplicationController
   def show
     @year = Year.find_by!(number: params[:year])
-    @participants = @year.participants.order(score: :desc).joins(:user).includes(:stars)
+    @participants = @year.participants.order(score: :desc).joins(:user).includes([:stars, :user])
     if current_user
       @participant = @year.participants.find_by(user: current_user)
     end
