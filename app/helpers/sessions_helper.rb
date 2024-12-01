9f4ae6567e2238795ec3d1659ffb3f9c87a72e04
Casper Schuijt <casper@cschuijt.nl>
2024-12-01 00:11:45 +0000

    log current user data for error tracing

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,12 +1,20 @@
 module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
+    Sentry.set_user({
+      id: user.id,
+      username: user.username
+    })
   end
 
   def current_user
     return unless session[:user_id]
 
     @current_user ||= User.find_by(id: session[:user_id])
+    Sentry.set_user({
+      id: @current_user.id,
+      username: @current_user.username
+    })
   end
 
   def logged_in?
@@ -16,6 +24,7 @@ module SessionsHelper
   def log_out
     session.delete(:user_id)
     @current_user = nil
+    Sentry.set_user({})
   end
 
   def current_user?(user)
