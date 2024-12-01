19069c518915acf313f77ca1099580613f957b4f
Casper Schuijt <casper@cschuijt.nl>
2024-12-01 16:20:36 +0000

    Simplify pages#home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,13 +3,8 @@ class PagesController < ApplicationController
     @year = Year.order(number: :desc).first
     if @year.nil?
       render 'pages/home'
-      return
+    else
+      redirect_to year_url(@year.number)
     end
-    @today = @year.days.where(open: true).order(number: :desc).first
-    @participants = @year.participants.order(score: :desc).joins(:user).includes(:stars)
-    if current_user
-      @participant = @year.participants.find_by(user: current_user)
-    end
-    render 'years/show'
   end
 end
