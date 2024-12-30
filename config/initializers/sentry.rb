59b23241b123392de7aa6cc255ed97d830952cf0
Casper Schuijt <casper@cschuijt.nl>
2024-12-30 15:36:15 +0000

    improve env var handling

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -6,9 +6,9 @@ if ENV["SENTRY_DSN"] && !ENV["SENTRY_DSN"].blank?
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
-    config.traces_sample_rate = ENV.fetch("SENTRY_TRACE_SAMPLE_RATE") { 1.0 }
+    config.traces_sample_rate = ENV.fetch("SENTRY_TRACE_SAMPLE_RATE") { 1.0 }&.to_f
 
     # Profile all requests, for funsies
-    config.profiles_sample_rate = ENV.fetch("SENTRY_PROFILE_SAMPLE_RATE") { 0.05 }
+    config.profiles_sample_rate = ENV.fetch("SENTRY_PROFILE_SAMPLE_RATE") { 0.05 }&.to_f
   end
 end
