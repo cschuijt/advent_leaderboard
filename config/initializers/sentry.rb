4be2c655902a547c9e9ae4919ed714eaca0d57fa
Casper Schuijt <casper@cschuijt.nl>
2024-12-30 15:08:52 +0000

    Do not profile all requests anymore because storage is finite apparently

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,4 @@
-if ENV["SENTRY_DSN"]
+if ENV["SENTRY_DSN"] && !ENV["SENTRY_DSN"].blank?
   Sentry.init do |config|
     config.dsn = ENV.fetch("SENTRY_DSN")
     config.breadcrumbs_logger = [:active_support_logger, :http_logger]
@@ -6,9 +6,9 @@ if ENV["SENTRY_DSN"]
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
-    config.traces_sample_rate = 1.0
+    config.traces_sample_rate = ENV.fetch("SENTRY_TRACE_SAMPLE_RATE") { 1.0 }
 
     # Profile all requests, for funsies
-    config.profiles_sample_rate = 1.0
+    config.profiles_sample_rate = ENV.fetch("SENTRY_PROFILE_SAMPLE_RATE") { 0.05 }
   end
 end
